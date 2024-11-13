a7086c1ec52170b56d97b5b29c6f90e5f15bec0d
Toan Tran <toan.tran@flynk.com>
2024-11-14 03:14:47 +0700

    Responsive for footer

--- src/components/DefaultLayoutFooter/index.tsx
+++ src/components/DefaultLayoutFooter/index.tsx
@@ -45,36 +45,30 @@ const DefaultLayoutFooter: FC = () => (
   <InvertColorConfigProvider>
     <Layout.Footer className="default-layout-footer-wrapper">
       <div className="default-layout-footer-wrapper-inner">
-        <Row gutter={48}>
-          <Col span={8}>
+        <Row gutter={[0, 48]}>
+          <Col xl={8} md={14} xs={24}>
             <Typography.Title level={4}>Navigation</Typography.Title>
-            <Row gutter={32}>
-              <Col>
-                {renderNavigationItems([
-                  NAVIGATION_ITEMS.HOME,
-                  NAVIGATION_ITEMS.ABOUT_US,
-                  NAVIGATION_ITEMS.OUR_TEAMS,
-                ])}
-              </Col>
-              <Col>
-                {renderNavigationItems([
-                  NAVIGATION_ITEMS.WHITEPAPER,
-                  NAVIGATION_ITEMS.MARKETPLACE,
-                  NAVIGATION_ITEMS.ROADMAP,
-                ])}
-              </Col>
-              <Col>
-                {renderNavigationItems([
-                  NAVIGATION_ITEMS.FAQS,
-                  NAVIGATION_ITEMS.NEWS,
-                  NAVIGATION_ITEMS.COMMUNITY,
-                ])}
-              </Col>
-            </Row>
+            <Flex gap={32}>
+              {renderNavigationItems([
+                NAVIGATION_ITEMS.HOME,
+                NAVIGATION_ITEMS.ABOUT_US,
+                NAVIGATION_ITEMS.OUR_TEAMS,
+              ])}
+              {renderNavigationItems([
+                NAVIGATION_ITEMS.WHITEPAPER,
+                NAVIGATION_ITEMS.MARKETPLACE,
+                NAVIGATION_ITEMS.ROADMAP,
+              ])}
+              {renderNavigationItems([
+                NAVIGATION_ITEMS.FAQS,
+                NAVIGATION_ITEMS.NEWS,
+                NAVIGATION_ITEMS.COMMUNITY,
+              ])}
+            </Flex>
           </Col>
-          <Col span={6}>
+          <Col xl={6} md={10} xs={24}>
             <Typography.Title level={4}>Contact Us</Typography.Title>
-            <Flex vertical gap={32}>
+            <Flex vertical gap={24}>
               <a href="tel:+01234568910" target="_blank" rel="noreferrer">
                 <Flex gap={8}>
                   <SvgIcon iconName="phone" />
@@ -93,7 +87,7 @@ const DefaultLayoutFooter: FC = () => (
               </a>
             </Flex>
           </Col>
-          <Col span={10}>
+          <Col xl={10} xs={24}>
             <Typography.Title level={4}>
               Subcribe to receive our latest update
             </Typography.Title>
@@ -106,18 +100,22 @@ const DefaultLayoutFooter: FC = () => (
           </Col>
         </Row>
         <Divider />
-        <Flex justify="space-between">
-          <Typography.Text>
-            ©2023 Tyme - Edit. All Rights reserved.
-          </Typography.Text>
-          <Flex gap={60}>
-            {[
-              NAVIGATION_ITEMS.SECURITY,
-              NAVIGATION_ITEMS.LEGAL,
-              NAVIGATION_ITEMS.PRIVACY,
-            ].map(renderNavigationItem)}
-          </Flex>
-        </Flex>
+        <Row justify="space-between" gutter={[0, 24]}>
+          <Col xs={24} md={12}>
+            <Typography.Text>
+              ©2023 Tyme - Edit. All Rights reserved.
+            </Typography.Text>
+          </Col>
+          <Col xs={24} md={12}>
+            <Flex gap={60} justify="flex-end" align="center">
+              {[
+                NAVIGATION_ITEMS.SECURITY,
+                NAVIGATION_ITEMS.LEGAL,
+                NAVIGATION_ITEMS.PRIVACY,
+              ].map(renderNavigationItem)}
+            </Flex>
+          </Col>
+        </Row>
       </div>
     </Layout.Footer>
   </InvertColorConfigProvider>
